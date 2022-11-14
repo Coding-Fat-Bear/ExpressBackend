9e05bbed6a9bbe52274585f9e968f311e557a37d
Lijoe Peter <lijoepeter.15@gmail.com>
2022-11-14 12:21:21 +0900

    few more updates

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-
+import {getLoginById,getLoginAll,delLoginById,createLogin,updateLogin} from "./servies.js";
 const app = express();
 
 var corsOptions = {
@@ -12,13 +12,25 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
-  res.json({
-     message: "Hello World" 
-    });
-app.use((err, req, res, next) => {
-      console.error(err.stack)
-    res.status(500).send( 'Something broke! ' )
-  })
+app.get("/data", async(req, res) => {
+  const [data] = await getLoginAll();
+  res.send(data);
 });
+
+app.get("/data/:id", async(req, res) => {
+  const id = req.params.id;
+  const [data] = await getLoginById(id);
+  res.send(data);
+});
+app.post("/create",async (req,res) => {
+  const {EMPCOD,USERNAME,PASSWORD} = req.body;
+  const data = await createLogin(EMPCOD,USERNAME,PASSWORD);
+  console.log(data);
+  res.json ({ message : "success"});
+})
+
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+res.status(500).send( 'Something broke! ' )
+})
 app.listen(3000,() => console.log('Started'));
--- servies.js
+++ servies.js
@@ -10,26 +10,24 @@ const connection = await mysql.createPool({
     queueLimit: 0
 });
 
-export async function getLoginById (i) {
-    return await connection.execute('SELECT * FROM `Login_Master` WHERE LOGINID = ?  ',i);
+export async function getLoginById (LOGINID) {
+    return await connection.execute('SELECT * FROM `Login_Master` WHERE LOGINID = ?  ',[LOGINID]);
 }
 
 export async function getLoginAll () {
     return await connection.execute('SELECT * FROM `Login_Master` ');
     }
 
-export async function delLoginById (i) {
-    return await connection.execute('DELETE FROM `Login_Master` WHERE (`LOGINID` = ?)',i);
+export async function delLoginById (LOGINID) {
+    return await connection.execute('DELETE FROM `Login_Master` WHERE (`LOGINID` = ?)',[LOGINID]);
 }
 
 
-export async function createLogin (i) {
-    return await connection.execute('INSERT INTO `Login_Master` (`LOGINID`, `EMPCOD`, `USERNAME`, `PASSWORD`) VALUES (?,?,?,?)',i);
+export async function createLogin (EMPCOD,USERNAME,PASSWORD) {
+    return await connection.execute('INSERT INTO `Login_Master` (`EMPCOD`, `USERNAME`, `PASSWORD`) VALUES (?,?,?)',[EMPCOD,USERNAME,PASSWORD]);
 }
-export async function updateLogin (i) {
-    return await connection.execute('UPDATE `Login_Master`  SET `USERNAME` = ?, `PASSWORD` = ?, `EMPCOD` = ? ' ,i);
+export async function updateLogin (LOGINID,USERNAME,PASSWORD) {
+    return await connection.execute('UPDATE `Login_Master`  SET `USERNAME` = ?, `PASSWORD` = ? ' ,[LOGINID,USERNAME,PASSWORD]);
 }
 
-let result = await delLoginById([12])
-console.log(result);
 
