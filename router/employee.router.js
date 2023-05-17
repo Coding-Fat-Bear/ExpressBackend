f71bf7f87bc0ac943f723f11df6a6bcefd1fadf0
Jayaraman Sriram <Jayaraman-Sriram@agnieconsulting.com>
2023-05-17 15:04:34 +0900

    employee master complete

--- router/employee.router.js
+++ router/employee.router.js
@@ -3,9 +3,9 @@ const router = require("express").Router();
 const employee = require("../controller/employee.controller");
 const { authcheckToken } = require("../Auth_tokn/auth_token.validation");
 
-router.post("/empins",authcheckToken,employee.empincont);
+router.post("/empins/:id/:APPMASTER/:AUTHFLG/",authcheckToken,employee.empincont);
 router.get("/empget/:id/:APPMASTER/:AUTHFLG/:EMPCOD",employee.empgetcont);
-router.put("/empupd",authcheckToken,employee.empupcont);
+router.put("/empupd/:id/:APPMASTER/:AUTHFLG/",authcheckToken,employee.empupcont);
 router.delete("/empdel",authcheckToken,employee.empdelcont);
 
 
